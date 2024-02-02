cc3f9defa04a4781992cf64f0a76f963c5b1f82c
Paulo <pdichone@gmail.com>
2024-02-02 15:47:24 -0800

    add navigation to movie details screen

---
+++ lib/pages/movie_details.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MovieDetails extends StatelessWidget {
+  const MovieDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movie Details'),
+      ),
+    );
+  }
+}
